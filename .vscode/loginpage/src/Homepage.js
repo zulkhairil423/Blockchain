1f3118e194210c33ec8443ffbf08118faf6f037f
zulkhairil423 <mzkhafiz@gmail.com>
2022-11-27 13:10:58 +0800

    Update Homepage.js

--- .vscode/loginpage/src/Homepage.js
+++ .vscode/loginpage/src/Homepage.js
@@ -8,9 +8,9 @@ import {
 } from "firebase/auth";
 import { auth } from "./firebaseconfig";
 import Autolog from "./artifacts/contracts/Autolog.sol/CarDataStorage.json";
-import { ethers } from "hardhat";
+import { ethers } from "ethers";
 
-const autologAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
+const autologAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
 
 function Homepage() {
   //property variables
