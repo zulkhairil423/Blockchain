cba6d13e368cb622370567c561e3a42f18e62efc
Roorero <72698539+Roorero@users.noreply.github.com>
2022-12-19 22:21:35 +0800

    Update FormPage.js

--- .vscode/loginpage/src/FormPage.js
+++ .vscode/loginpage/src/FormPage.js
@@ -7,6 +7,10 @@ import {
 import { auth } from "./firebaseconfig";
 import { useState, useEffect } from  "react";
 import {useHistory} from "react-router-dom";
+import { ethers } from "ethers";
+import CarDataStorage from "./artifacts/contracts/Autolog.sol/CarDataStorage.json";
+
+const autologAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
 
 function FormPage() {
     const [date, setDate] = useState("");
@@ -27,7 +31,24 @@ function FormPage() {
       };
  
     const submit = async () => {
+      if (!detail) return;
+      if (!date) return;
+      if (!fullname) return;
 
+      const provider = new ethers.providers.JsonRpcProvider();
+
+      const contract = new ethers.Contract(autologAddress, CarDataStorage.abi, provider);
+      
+      const report = detail.concat("\n\n\n", fullname, "\n", date);
+      
+      const transaction = await contract.updateCar(report);
+
+      setReportDetail("");
+      setDate("");
+      setFullName("");
+
+      await transaction.wait();
+      console.log("Details submitted successfully");
     };
     
     return(
